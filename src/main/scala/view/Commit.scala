93a02a0cffc22f8a7d6c3aa47714170a70d76c15
Al153 <al.taylor1105@gmail.com>
2017-10-16 22:41:08 +0100

    Added view and commit traits

---
+++ src/main/scala/view/Commit.scala
@@ -0,0 +1,8 @@
+package view
+
+/**
+  * Created by Al on 07/10/2017.
+  */
+case class Commit(id: Long) extends AnyVal {
+
+}
---
+++ src/main/scala/view/View.scala
@@ -0,0 +1,21 @@
+package view
+
+import core.error.E
+import core.containers.{ConstrainedFuture, Operation}
+
+/**
+  * Created by Al on 13/10/2017.
+  *
+  * This is a class that handles all inputs and outputs to a view of the database.
+  *
+  * This should abstract away things like garbage collection in the database, and compile
+  * queries to the underlying SQL
+  */
+trait View {
+  val id: Long
+  val dependents: Set[View]
+
+  def diff: Operation[E, Diff] = Operation{v: View => new ConstrainedFuture[E, (Diff, View)](???)(???)}
+
+  def execute[A](q: Operation[E, A]): ConstrainedFuture[E, (A, View)] = q.runView(this)
+}
