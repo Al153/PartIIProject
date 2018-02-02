05b9fae00ac87f7558f64f84cb3893ebe433fdbb
Al153 <al.taylor1105@gmail.com>
2018-02-02 23:14:47 +0000

    Added writes and SQL tests to test harness

---
+++ src/main/scala/remote/tests/FindSingles.scala
@@ -0,0 +1,42 @@
+package remote.tests
+
+import construction.imdb.IMDBSchema.{ActsIn, Person}
+import core.user.containers.ConstrainedFuture
+import core.user.dsl._
+import core.user.interfaces.DBInstance
+import core.user.schema.SchemaDescription
+import remote.{TestIndex, TestName, TestSpec}
+import TestIndex._
+import TestName._
+import construction.imdb.{DBBuilder, IMDBSchema}
+
+import scala.concurrent.ExecutionContext
+
+
+/**
+  * Created by Al on 02/02/2018.
+  */
+object FindSingles extends TestSpec[Set[(Person, Person)]] {
+  override def testName: TestName = "FindSingles".test
+
+  override def batchSize: TestIndex = 10.tests
+
+  override def setup(d: DBInstance)(implicit ec: ExecutionContext): ConstrainedFuture[E, Unit] = {
+    implicit val inst: DBInstance = d
+
+    using(d){
+      DBBuilder.buildDB("imdb/large")
+    }
+  }
+
+
+  override def test(d: DBInstance)(index: TestIndex)(implicit ec: ExecutionContext): ConstrainedFuture[E, Set[(Person, Person)]] =
+    {
+      implicit val inst = d
+      readDefault(inst) {
+        findPairs(((ActsIn -->> (KevinBacon >> ActsIn)) --><-- ActsIn).*(0 --> index.i))
+      }
+    }
+
+  override def schema: SchemaDescription = IMDBSchema.schemaDescription
+}
---
+++ src/main/scala/remote/tests/Writes.scala
@@ -0,0 +1,46 @@
+package remote.tests
+
+import construction.imdb.{DBBuilder, IMDBSchema}
+import core.user.containers.ConstrainedFuture
+import core.user.dsl._
+import core.user.interfaces.DBInstance
+import core.user.schema.SchemaDescription
+import remote.{TestIndex, TestName, TestSpec}
+import TestIndex._
+import TestName._
+
+import scala.concurrent.ExecutionContext
+
+/**
+  * Created by Al on 02/02/2018.
+  */
+object Writes extends TestSpec[Unit]{
+  override def testName: TestName = "WriteSpeed".test
+
+  override def batchSize: TestIndex = 7.tests
+
+  override def setup(d: DBInstance)(implicit ec: ExecutionContext): ConstrainedFuture[E, Unit] = ConstrainedFuture.point(()) {
+    e: Throwable => new E {
+      override def toString: String = e.getMessage
+    }
+  }
+
+
+  override def test(d: DBInstance)(index: TestIndex)(implicit ec: ExecutionContext): ConstrainedFuture[E, Unit] = {
+    implicit val inst: DBInstance = d
+    readDefault(d){
+      index.i match {
+        case 1 => DBBuilder.buildDB("imdb/smallest")
+        case 2 => DBBuilder.buildDB("imdb/smallest")
+        case 3 => DBBuilder.buildDB("imdb/smallest")
+        case 4 => DBBuilder.buildDB("imdb/smallest")
+        case 5 => DBBuilder.buildDB("imdb/smallest")
+        case 6 => DBBuilder.buildDB("imdb/smallest")
+        case _ => DBBuilder.buildDB("imdb/smallest")
+      }
+    }
+  }
+
+
+  override def schema: SchemaDescription = IMDBSchema.schemaDescription
+}
