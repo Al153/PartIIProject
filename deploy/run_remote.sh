2247cd4014a90b09a67f00d9e0d8354d45791614
Al153 <al.taylor1105@gmail.com>
2018-02-11 14:36:49 +0000

    Run unit tests as performance tests run

--- deploy/run_remote.sh
+++ deploy/run_remote.sh
@@ -4,6 +4,7 @@ git pull origin master
 SHA="$(git show -s --format=%H| cut -c1-8)"
 echo ${SHA}
 sbt compile
+sbt test 2>&1 | tee ~/perfTests/lastTests.log
 sbt "runMain remote.Run" 2>&1 | tee  ~/perfTests/${SHA}.log | tee ~/perfTests/last.log
 
 
