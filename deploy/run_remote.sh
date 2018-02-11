44109157214685d408e8256dac4a9f8dd0c17754
Al153 <al.taylor1105@gmail.com>
2018-02-11 14:16:42 +0000

    Log to multiple locations

--- deploy/run_remote.sh
+++ deploy/run_remote.sh
@@ -4,5 +4,6 @@ git pull origin master
 SHA="$(git show -s --format=%H| cut -c1-8)"
 echo ${SHA}
 sbt compile
-sbt "runMain remote.Run" 2>&1 | tee  ~/perfTests/${SHA}.log
+sbt "runMain remote.Run" 2>&1 | tee  ~/perfTests/${SHA}.log | tee ~/perfTests/last.log
+
 
