014a2fcdd6cf6a7b30ce1dbb2571f75c77464d9e
Al153 <al.taylor1105@gmail.com>
2018-02-19 22:58:25 +0000

    Store last cha value

--- deploy/run_remote.sh
+++ deploy/run_remote.sh
@@ -3,7 +3,7 @@
 git pull origin master
 SHA="$(git show -s --format=%H| cut -c1-8)"
 
-echo ${SHA} > ~perfTests/lastSha.log
+echo ${SHA} > ~/perfTests/lastSha.log
 sbt compile
 sbt test 2>&1 | tee ~/perfTests/lastTests.log
 sbt "runMain remote.Run" 2>&1 | tee  ~/perfTests/${SHA}.log | tee ~/perfTests/last.log
