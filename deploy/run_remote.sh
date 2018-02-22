151c7f38c6f44e827d365a32e8573951cb0a3aea
at736 <al.taylor1105@live.co.uk>
2018-02-22 10:40:24 +0000

    kerberos-safe logging and RawLookupLarge made smaller

--- deploy/run_remote.sh
+++ deploy/run_remote.sh
@@ -5,9 +5,9 @@ SHA="$(git show -s --format=%H| cut -c1-8)"
 tmpDir="/local/scratch-2/at736"
 
 
-echo ${SHA} > ~/${tmpDir}/lastSha.log
+echo ${SHA} > ${tmpDir}/lastSha.log
 sbt compile
 sbt test 2>&1 | tee ~/${tmpDir}/lastTests.log
-sbt "runMain remote.Run" 2>&1 | tee  ~/${tmpDir}/${SHA}.log | tee ~/perfTests/last.log
+sbt "runMain remote.Run" 2>&1 | tee ${tmpDir}/${SHA}.log | tee ${tmpDir}/last.log
 
 
