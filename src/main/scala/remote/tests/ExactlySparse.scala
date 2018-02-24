f8aaf248dc6fc36649ec9e5a1f51fee7373558d1
Al153 <al.taylor1105@gmail.com>
2018-02-24 10:24:04 +0000

    Added more exactly tests

---
+++ src/main/scala/remote/tests/ExactlySparse.scala
@@ -0,0 +1,40 @@
+package remote.tests
+
+import construction.imdb.DBBuilder
+import construction.imdb.IMDBSchema.{ActsIn, Person, schemaDescription}
+import core.user.containers.ConstrainedFuture
+import core.user.dsl._
+import core.user.interfaces.DBInstance
+import core.user.schema.SchemaDescription
+import remote.{lmdbOriginal, logger}
+import remote.util.{TestIndex, TestSpec}
+
+import scala.concurrent.ExecutionContext
+import TestIndex._
+import remote.util.TestName._
+
+/**
+  * Created by Al on 24/02/2018.
+  */
+
+object ExactlySparse extends TestSpec[Set[Person]] {
+  override def testName = "Exactly".test
+  override def schema: SchemaDescription = schemaDescription
+  override  def setup(instance: DBInstance)(implicit ec: ExecutionContext): ConstrainedFuture[E, Unit] =
+    using(instance){
+      DBBuilder.buildDB("imdb/smallest")(instance)
+    }
+
+  override def test(instance: DBInstance)(index: TestIndex)(implicit ec: ExecutionContext): ConstrainedFuture[E, Set[Person]] =
+    for { res <- {
+      implicit val inst = instance
+      using(instance){
+        find(KevinBacon >> ((ActsIn --><-- ActsIn) * index.i))
+      }
+    }
+          _ = logger.info("Length of exactlies = " + res.size)
+    } yield res
+
+  override def batchSize: TestIndex = 5.tests
+  override def ignoreBackends: Set[String] = Set(lmdbOriginal)
+}
