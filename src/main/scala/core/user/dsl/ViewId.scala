a10a49dc2bf420a2a49561fbe363af32cace5206
at736 <al.taylor1105@live.co.uk>
2018-02-27 16:59:40 +0000

    View => ViewId

--- src/main/scala/core/user/dsl/ViewId.scala
+++ src/main/scala/core/user/dsl/ViewId.scala
@@ -2,6 +2,6 @@ package core.user.dsl
 
 /**
   * Created by Al on 13/10/2017.
-  * A view id is the central part of maintaining the immutability of the database.
+  * A view id is the central part of maintaining the immutability of the database
   */
 case class ViewId(id: Long) extends AnyVal
